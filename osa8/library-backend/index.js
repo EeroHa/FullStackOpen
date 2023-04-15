eb268632472cb0aa749b04c732cb6c6887d7574c
Eero Hakanen <eero.v.hakanen@tuni.fi>
2023-04-15 12:40:34 +0300

    library users and login

--- osa8/library-backend/index.js
+++ osa8/library-backend/index.js
@@ -1,11 +1,13 @@
 const { ApolloServer } = require('@apollo/server');
 const { startStandaloneServer } = require('@apollo/server/standalone');
 const { GraphQLError } = require('graphql');
+const jwt = require('jsonwebtoken');
 
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
 const Book = require('./models/Book');
 const Author = require('./models/Authors');
+const User = require('./models/User');
 
 require('dotenv').config();
 
@@ -37,11 +39,22 @@ const typeDefs = `
       bookCount: Int!
   }
 
+  type User {
+    username: String!
+    favoriteGenre: String!
+    id: ID!
+  }
+  
+  type Token {
+    value: String!
+  }
+
   type Query {
     bookCount: Int,
     authorCount: Int,
     allBooks(genre: String): [Book]!
     allAuthors: [Author!]!
+    me: User
   }
 
   type Mutation {
@@ -52,6 +65,14 @@ const typeDefs = `
         genres: [String!]!
     ): Book!,
     editAuthor(name: String!, setBornTo: Int!): Author
+    createUser(
+      username: String!
+      favoriteGenre: String!
+    ): User
+    login(
+      username: String!
+      password: String!
+    ): Token
 }
 `;
 
@@ -76,9 +97,21 @@ const resolvers = {
       );
       return authorsWithBookCount;
     },
+    me: (root, args, context) => {
+      return context.currentUser;
+    },
   },
   Mutation: {
-    addBook: async (_, args) => {
+    addBook: async (_, args, context) => {
+      const currentUser = context.currentUser;
+      if (!currentUser) {
+        throw new GraphQLError('not authenticated', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+          },
+        });
+      }
+
       const author = await Author.findOne({ name: args.author });
       if (!author) {
         const newAuthor = new Author({ name: args.author });
@@ -109,7 +142,16 @@ const resolvers = {
       }
       return book;
     },
-    editAuthor: async (_, args) => {
+    editAuthor: async (_, args, context) => {
+      const currentUser = context.currentUser;
+      if (!currentUser) {
+        throw new GraphQLError('not authenticated', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+          },
+        });
+      }
+
       const { name, setBornTo } = args;
       const author = await Author.findOne({ name: name });
       author.born = setBornTo;
@@ -126,6 +168,41 @@ const resolvers = {
       }
       return author;
     },
+    createUser: async (_, { username, favoriteGenre }) => {
+      const user = new User({
+        username: username,
+        favoriteGenre: favoriteGenre,
+      });
+
+      return user.save().catch((error) => {
+        throw new GraphQLError('Creating the user failed', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+            invalidArgs: args.name,
+            error,
+          },
+        });
+      });
+    },
+
+    login: async (_, { username, password }) => {
+      const user = await User.findOne({ username: username });
+
+      if (!user || password !== 'secret') {
+        throw new GraphQLError('wrong credentials', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+          },
+        });
+      }
+
+      const userForToken = {
+        username: user.username,
+        id: user._id,
+      };
+
+      return { value: jwt.sign(userForToken, process.env.JWT_SECRET) };
+    },
   },
 };
 
@@ -136,6 +213,17 @@ const server = new ApolloServer({
 
 startStandaloneServer(server, {
   listen: { port: 4000 },
+  context: async ({ req, res }) => {
+    const auth = req ? req.headers.authorization : null;
+    if (auth && auth.startsWith('Bearer ')) {
+      const decodedToken = jwt.verify(
+        auth.substring(7),
+        process.env.JWT_SECRET
+      );
+      const currentUser = await User.findById(decodedToken.id);
+      return { currentUser };
+    }
+  },
 }).then(({ url }) => {
   console.log(`Server ready at ${url}`);
 });
